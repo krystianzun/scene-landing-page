b0257e861d87b0f2faaadb1f4824a24504013db9
krystianzun <44001786+krystianzun@users.noreply.github.com>
2024-08-30 10:43:54 +0100

    Shorten the copy

--- app/ManifestoSection.tsx
+++ app/ManifestoSection.tsx
@@ -7,30 +7,30 @@ const ManifestoSection = () => {
     <section>
       <div className="text-center flex flex-col gap-8 text-xl mb-12">
         <p>
-          We believe the internet needs a straightforward, non-commercial 3D
-          viewer—a tool that lets anyone share and view 3D models with ease.
+          We believe the internet needs a simple, non-commercial 3D viewer—a
+          tool that lets anyone share and view 3D models easily.
         </p>
         <p>
-          Our world is 3D, yet we&apos;re still stuck sharing and viewing
-          content on flat, 2D screens. It doesn&apos;t make sense.
+          Our world is 3D, yet we&apos;re still using flat, 2D screens. It
+          doesn&apos;t make sense.
         </p>
         <p>
-          Current tools for sharing 3D content are either too complicated or too
-          limited. Whether you&apos;re a creator showcasing your work or a user
-          exploring, the barriers are frustrating. With AI-generated 3D models
-          and XR technologies on the rise, the need for easy 3D sharing is only
-          growing. But there&apos;s a huge gap—a lack of simple, intuitive tools
-          for bringing these creations to life online.
+          Current tools are either too complex or too limited. Whether
+          you&apos;re a creator or a user, it&apos;s difficult to just share or
+          explore 3D content.
         </p>
         <p>
-          We&apos;re here to change that. Our mission is clear: to create a
-          platform that makes sharing and viewing 3D models quick and easy. No
-          complicated software, no confusing interfaces—just a clean, powerful
-          tool anyone can use. We want to make it effortless for creators to
-          share their vision and for users to dive into 3D content.
+          With AI-generated 3D models and XR on the rise, the need for easy 3D
+          sharing is only growing. But there&apos;s a huge gap — a lack of
+          simple, intuitive tools for bringing these creations to life.
+        </p>
+        <p>
+          We&apos;re here to change that. Our mission is to create a platform
+          that makes sharing and viewing 3D models effortless—no complex
+          software, just a clean, powerful tool anyone can use.
         </p>
       </div>
-      <div className="text-center flex flex-col gap-16 text-xl items-center justify-center">
+      <div className="text-center flex flex-col gap-12 text-xl items-center justify-center">
         <p>Join our Discord to try it out and follow the progress!</p>
         <a
           href="https://www.youtube.com/"
